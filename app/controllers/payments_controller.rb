b806cf7a4b9fe856593a92d16ba41a03a0ad2e71
Rick Peyton <peytorb@gmail.com>
2015-06-18 12:02:33 -0500

    auth working with hidden fields

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,25 +3,43 @@ class PaymentsController < ApplicationController
   layout 'authorize_net'
   helper :authorize_net
   protect_from_forgery :except => :relay_response
+
+  # Available transaction_type(s)
+    # AUTHORIZE_AND_CAPTURE = "AUTH_CAPTURE"
+    # AUTHORIZE_ONLY = "AUTH_ONLY"
+    # CAPTURE_ONLY = "CAPTURE_ONLY"
+    # CREDIT = "CREDIT"
+    # PRIOR_AUTHORIZATION_AND_CAPTURE = "PRIOR_AUTH_CAPTURE"
+    # VOID = "VOID"
+
+  # Test Card Numbers
+    # American Express Test Card 370000000000002
+    # Discover Test Card 6011000000000012
+    # JCB 3088000000000017
+    # Diners Club/ Carte Blanche 38000000000006
+    # MasterCard 5424000000000015
+    # Visa Test Card 4007000000027 or 4012888818888
+
   NGROK = "http://84df53b0.ngrok.io"
 
   # GET
   # Displays a payment form.
   def payment
     @amount = 10.00
+
     @sim_transaction = AuthorizeNet::SIM::Transaction.new(
       AUTHORIZE_NET_CONFIG['api_login_id'],
       AUTHORIZE_NET_CONFIG['api_transaction_key'],
       @amount, :relay_url => NGROK + "/payments/relay_response",
       :transaction_type => "AUTH_ONLY"
     )
+    @hidden_fields = hidden_fields
   end
 
   # POST
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
     sim_response = AuthorizeNet::SIM::Response.new(params)
-    binding.pry
     if sim_response.success?(
       AUTHORIZE_NET_CONFIG['api_login_id'],
       AUTHORIZE_NET_CONFIG['merchant_hash_value']
@@ -39,6 +57,30 @@ class PaymentsController < ApplicationController
   # Displays a receipt.
   def receipt
     @auth_code = params[:x_auth_code]
+  end
+
+  def hidden_fields
+    {
+      x_invoice_num: "ORDER_NUMBER",
+      x_description: "AO Proof of concept authorization",
+      x_first_name: "John",
+      x_last_name: "Doe",
+      x_address: "2427 1st Avenue North",
+      x_city: "Birmingham",
+      x_state: "AL",
+      x_zip: "35203",
+      x_phone: "205-538-0240",
+      x_email: "rick@motionmobs.com",
+      x_cust_id: "USER_ID",
+      x_ship_to_first_name: "",
+      x_ship_to_last_name: "",
+      x_ship_to_company: "",
+      x_ship_to_address: "",
+      x_ship_to_city: "",
+      x_ship_to_state: "",
+      x_ship_to_zip: "",
+      x_po_num: "ORDER_NUMBER"
+    }
   end
 
 end
