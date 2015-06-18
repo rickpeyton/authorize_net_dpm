04f4e5b8fab5ca45ad2025e1475417b08bd5c37c
Rick Peyton <peytorb@gmail.com>
2015-06-18 10:14:31 -0500

    successful auth_only transaction

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -3,6 +3,7 @@ class PaymentsController < ApplicationController
   layout 'authorize_net'
   helper :authorize_net
   protect_from_forgery :except => :relay_response
+  NGROK = "http://84df53b0.ngrok.io"
 
   # GET
   # Displays a payment form.
@@ -11,7 +12,8 @@ class PaymentsController < ApplicationController
     @sim_transaction = AuthorizeNet::SIM::Transaction.new(
       AUTHORIZE_NET_CONFIG['api_login_id'],
       AUTHORIZE_NET_CONFIG['api_transaction_key'],
-      @amount, :relay_url => payments_relay_response_url(:only_path => false)
+      @amount, :relay_url => NGROK + "/payments/relay_response",
+      :transaction_type => "AUTH_ONLY"
     )
   end
 
@@ -19,6 +21,7 @@ class PaymentsController < ApplicationController
   # Returns relay response when Authorize.Net POSTs to us.
   def relay_response
     sim_response = AuthorizeNet::SIM::Response.new(params)
+    binding.pry
     if sim_response.success?(
       AUTHORIZE_NET_CONFIG['api_login_id'],
       AUTHORIZE_NET_CONFIG['merchant_hash_value']
